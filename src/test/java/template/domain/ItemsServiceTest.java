d09409bb1e0b86d5c558e4e8209a76697124683d
Kamil Mazurek <42776173+kamilmazurek@users.noreply.github.com>
2025-08-24 16:56:18 +0200

    Add handling for GET by ID requests

--- src/test/java/template/domain/ItemsServiceTest.java
+++ src/test/java/template/domain/ItemsServiceTest.java
@@ -1,9 +1,13 @@
 package template.domain;
 
 import org.junit.jupiter.api.Test;
+import template.persistence.ItemEntity;
 import template.persistence.ItemsRepository;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -14,8 +18,50 @@ import static org.springframework.test.util.ReflectionTestUtils.invokeMethod;
 
 public class ItemsServiceTest {
 
+
     @Test
-    void shouldReturnItems() {
+    void shouldGetItem() {
+        //given item
+        var item = ItemEntity.builder().id(1L).name("Item A").build();
+
+        //and repository
+        var repository = mock(ItemsRepository.class);
+        when(repository.findById(item.getId())).thenReturn(Optional.of(item));
+
+        //and service
+        var service = new ItemsService(repository);
+
+        //when item is requested
+        var result = service.getItem(item.getId());
+
+        //then expected item is returned
+        assertEquals(Optional.of(invokeMethod(service, "toDomainObject", item)), result);
+
+        //and repository was queried for data
+        verify(repository).findById(item.getId());
+    }
+
+    @Test
+    void shouldNotFindItem() {
+        //given repository
+        var repository = mock(ItemsRepository.class);
+        when(repository.findById(1L)).thenReturn(Optional.empty());
+
+        //and service
+        var service = new ItemsService(repository);
+
+        //when item is requested
+        var result = service.getItem(1L);
+
+        //then no items are returned
+        assertTrue(result.isEmpty());
+
+        //and repository was queried for data
+        verify(repository).findById(1L);
+    }
+
+    @Test
+    void shouldGetItems() {
         //given repository
         var repository = mock(ItemsRepository.class);
         when(repository.findAll()).thenReturn(createTestItemEntities());
