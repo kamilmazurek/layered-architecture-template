d00a2a57d9df026b19a8e8087a2bb3e6c41dfeb2
Kamil Mazurek <42776173+kamilmazurek@users.noreply.github.com>
2025-08-24 15:28:06 +0200

    Handling of PUT request added

--- src/test/java/template/api/ItemsControllerIntegrationTest.java
+++ src/test/java/template/api/ItemsControllerIntegrationTest.java
@@ -1,27 +1,65 @@
 package template.api;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import template.AbstractIntegrationTest;
 import template.api.model.ItemDTO;
 
+import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.when;
 import static java.util.Arrays.asList;
+import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static template.util.TestItems.createTestItemDTOs;
 
 public class ItemsControllerIntegrationTest extends AbstractIntegrationTest {
 
+    private final ObjectWriter objectWriter = new ObjectMapper().writer();
+
     @Test
-    void shouldReturnItems() {
-        //when items are requested
-        var response = when().get("/items");
-
-        //then expected items are returned
-        assertEquals(createTestItemDTOs(), asList(response.as(ItemDTO[].class)));
-
-        //and OK status code is returned
-        assertEquals(HttpStatus.OK.value(), response.getStatusCode());
+    void shouldReturnItems() throws JsonProcessingException {
+        when()
+                .get("/items")
+                .then()
+                .statusCode(200)
+                .assertThat()
+                .body(equalTo(objectWriter.writeValueAsString(createTestItemDTOs())));
     }
+
+    @Test
+    void shouldInsertItemByPutRequest() {
+        //given item
+        var item = new ItemDTO().id(4L).name("Item D");
+
+        //when PUT request with item is sent
+        given()
+                .contentType("application/json")
+                .body(item)
+                .when()
+                .put("/items/4")
+                .then()
+                .statusCode(200);
+
+        //then item can be retrieved by ID
+        // TODO: Once GET by ID is implemented, retrieve the item and verify it matches the previously stored one.
+
+        //cleanup
+        // TODO: Add cleanup once DELETE by ID is implemented.
+    }
+
+    @Test
+    void shouldNotAcceptPutRequestWhenItemHasAmbiguousID() {
+        given()
+                .contentType("application/json")
+                .body(new ItemDTO().id(5L).name("Item E"))
+                .when()
+                .put("/items/6")
+                .then()
+                .statusCode(400);
+    }
+
 
 }
--- src/test/java/template/domain/ItemsServiceTest.java
+++ src/test/java/template/domain/ItemsServiceTest.java
@@ -10,6 +10,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static template.util.TestItems.createTestItemEntities;
 import static template.util.TestItems.createTestItems;
+import static org.springframework.test.util.ReflectionTestUtils.invokeMethod;
 
 public class ItemsServiceTest {
 
@@ -30,6 +31,24 @@ public class ItemsServiceTest {
 
         //and repository was involved in retrieving the data
         verify(repository, times(1)).findAll();
+    }
+
+    @Test
+    void shouldPutItem() {
+        //given repository
+        var repository = mock(ItemsRepository.class);
+
+        //and service
+        var service = new ItemsService(repository);
+
+        //and item
+        var item = Item.builder().name("Item A").build();
+
+        //when item is put
+        service.putItem(1L, item);
+
+        //then item has been put to repository
+        verify(repository, times(1)).save(invokeMethod(service, "toEntity", item));
     }
 
 }
