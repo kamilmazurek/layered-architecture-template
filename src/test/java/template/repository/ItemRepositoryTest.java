041d19e080043eefb5ee16b3e95534f395b9e032
Kamil Mazurek <42776173+kamilmazurek@users.noreply.github.com>
2026-02-20 00:34:27 +0100

    Improve ItemRepository and ItemRepositoryTest

--- src/test/java/template/repository/ItemRepositoryTest.java
+++ src/test/java/template/repository/ItemRepositoryTest.java
@@ -14,169 +14,157 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.startsWith;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static template.repository.Queries.ALTER_SEQUENCE_QUERY;
+import static template.repository.Queries.CURRENT_SEQ_VAL_QUERY;
+import static template.repository.Queries.LOCK_QUERY;
+import static template.repository.Queries.MERGE_QUERY;
 import static template.util.TestItems.createTestItemEntities;
 
 class ItemRepositoryTest {
 
-    private final String LOCK_QUERY_STRING = "SELECT 1 FROM ITEM_SEQ_LOCK FOR UPDATE";
-
     @Test
     void shouldFindItemById() {
-        // given entity
+        //given entity
         var entity = ItemEntity.builder().id(1L).name("Item A").build();
 
-        // and JPA repository
+        //and JPA repository
         var jpaRepository = mock(ItemJpaRepository.class);
         when(jpaRepository.findById(1L)).thenReturn(Optional.of(entity));
 
-        // and entity manager
-        var entityManager = mock(EntityManager.class);
+        //and repository
+        var repository = new ItemRepository(mock(EntityManager.class), jpaRepository);
 
-        // and repository
-        var repository = new ItemRepository(entityManager, jpaRepository);
-
-        // when item is requested
+        //when item is requested
         var result = repository.findById(1L);
 
-        // then expected item is returned
+        //then expected item is returned
         assertEquals(Optional.of(entity), result);
 
-        // and repository was queried for data
+        //and repository was queried for data
         verify(jpaRepository).findById(1L);
     }
 
     @Test
     void shouldFindAllItems() {
-        // given entities
+        //given entities
         var entities = createTestItemEntities();
 
-        // and JPA repository
+        //and JPA repository
         var jpaRepository = mock(ItemJpaRepository.class);
         when(jpaRepository.findAll()).thenReturn(entities);
 
-        // and entity manager
-        var entityManager = mock(EntityManager.class);
+        //and repository
+        var repository = new ItemRepository(mock(EntityManager.class), jpaRepository);
 
-        // and repository
-        var repository = new ItemRepository(entityManager, jpaRepository);
-
-        // when items are requested
+        //when items are requested
         var result = repository.findAll();
 
-        // then items are returned
+        //then items are returned
         assertEquals(entities, result);
 
-        // and repository was queried for data
+        //and repository was queried for data
         verify(jpaRepository).findAll();
     }
 
     @Test
     void shouldCreateItem() {
-        // given entity
+        //given entity
         var entity = ItemEntity.builder().name("Item A").build();
 
-        // and JPA repository
+        //and JPA repository
         var jpaRepository = mock(ItemJpaRepository.class);
 
-        // and entity manager
+        //and entity manager
         var entityManager = mock(EntityManager.class);
         var lockQuery = createLockQuery(entityManager);
 
-        // and repository
+        //and repository
         var repository = new ItemRepository(entityManager, jpaRepository);
 
-        // when item is created
+        //when item is created
         repository.create(entity);
 
-        // then entity manager is used for lock
-        verify(entityManager).createNativeQuery(LOCK_QUERY_STRING);
+        //then lock query is executed
+        verify(entityManager).createNativeQuery(LOCK_QUERY);
         verify(lockQuery).getResultList();
 
-        // and item is saved in JPA repository
+        //and item is saved in repository
         verify(jpaRepository).save(entity);
     }
 
     @Test
     void shouldUpsertItem() {
-        // given entity
-        var entity = ItemEntity.builder().name("Item A").build();
+        //given entity
+        var entity = ItemEntity.builder().id(1L).name("Item A").build();
 
-        // and JPA repository
+        //and JPA repository
         var jpaRepository = mock(ItemJpaRepository.class);
 
-        // and entity manager with helper methods for queries
+        //and entity manager with helper methods for queries
         var entityManager = mock(EntityManager.class);
         var lockQuery = createLockQuery(entityManager);
         var mergeQuery = createMergeQuery(entityManager);
-        var seqQuery = createSequenceQuery(entityManager, 0L);
-        createAlterSequenceQuery(entityManager);
+        var seqQuery = createSequenceQuery(entityManager);
+        createAlterSequenceQuery(entityManager, 2L);
 
-        // and repository
+        //and repository
         var repository = new ItemRepository(entityManager, jpaRepository);
 
-        // when item is upserted
-        repository.upsert(1L, entity);
+        //when item is upserted
+        repository.upsert(entity.getId(), entity);
 
-        // then lock query is executed
-        verify(entityManager).createNativeQuery(LOCK_QUERY_STRING);
+        //then lock query is executed
+        verify(entityManager).createNativeQuery(LOCK_QUERY);
         verify(lockQuery).getResultList();
 
-        // and merge query is executed
+        //and merge query is executed
         verify(entityManager).createNativeQuery("MERGE INTO item (id, name) KEY(id) VALUES (?, ?)");
-        verify(mergeQuery).setParameter(1, 1L);
+        verify(mergeQuery).setParameter(1, entity.getId());
         verify(mergeQuery).setParameter(2, entity.getName());
 
-        // and sequence query is executed
+        //and sequence query is executed
         verify(seqQuery).getSingleResult();
         verify(entityManager).createNativeQuery(startsWith("ALTER SEQUENCE ITEM_SEQ RESTART WITH"));
     }
 
     @Test
     void shouldDeleteItem() {
-        // given JPA repository
+        //given JPA repository
         var jpaRepository = mock(ItemJpaRepository.class);
+        //and repository
+        var repository = new ItemRepository(mock(EntityManager.class), jpaRepository);
 
-        // and entity manager
-        var entityManager = mock(EntityManager.class);
-
-        // and repository
-        var repository = new ItemRepository(entityManager, jpaRepository);
-
-        // when item is deleted
+        //when item is deleted
         repository.deleteById(1L);
 
-        // then item is deleted from JPA repository
+        //then item is deleted from JPA repository
         verify(jpaRepository).deleteById(1L);
     }
 
     private Query createLockQuery(EntityManager em) {
         var query = mock(Query.class);
-        when(em.createNativeQuery(LOCK_QUERY_STRING)).thenReturn(query);
+        when(em.createNativeQuery(LOCK_QUERY)).thenReturn(query);
         when(query.getResultList()).thenReturn(List.of(1));
         return query;
     }
 
     private Query createMergeQuery(EntityManager em) {
-        var mergeQueryString = "MERGE INTO item (id, name) KEY(id) VALUES (?, ?)";
         var query = mock(Query.class);
-        when(em.createNativeQuery(mergeQueryString)).thenReturn(query);
+        when(em.createNativeQuery(MERGE_QUERY)).thenReturn(query);
         when(query.setParameter(anyInt(), any())).thenReturn(query);
         return query;
     }
 
-    private Query createSequenceQuery(EntityManager em, long currentValue) {
-        var seqQueryString = "SELECT CAST(BASE_VALUE AS BIGINT) FROM INFORMATION_SCHEMA.SEQUENCES WHERE SEQUENCE_NAME = 'ITEM_SEQ'";
+    private Query createSequenceQuery(EntityManager em) {
         var query = mock(Query.class);
-        when(em.createNativeQuery(seqQueryString)).thenReturn(query);
-        when(query.getSingleResult()).thenReturn(currentValue);
+        when(em.createNativeQuery(CURRENT_SEQ_VAL_QUERY)).thenReturn(query);
+        when(query.getSingleResult()).thenReturn(0L);
         return query;
     }
 
-    private Query createAlterSequenceQuery(EntityManager em) {
-        var alterSeqPrefix = "ALTER SEQUENCE ITEM_SEQ RESTART WITH";
+    private void createAlterSequenceQuery(EntityManager em, Long id) {
         var query = mock(Query.class);
-        when(em.createNativeQuery(startsWith(alterSeqPrefix))).thenReturn(query);
-        return query;
+        when(em.createNativeQuery(String.format(ALTER_SEQUENCE_QUERY, id))).thenReturn(query);
     }
 
 }
