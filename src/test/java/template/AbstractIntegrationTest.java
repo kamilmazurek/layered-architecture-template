4877b7d830d028096b7a18ef12f0afbb3d8a8614
Kamil Mazurek <42776173+kamilmazurek@users.noreply.github.com>
2026-06-07 21:51:03 +0200

    Simplify test configuration

--- src/test/java/template/AbstractIntegrationTest.java
+++ src/test/java/template/AbstractIntegrationTest.java
@@ -4,9 +4,11 @@ import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
+@ActiveProfiles("test")
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 public abstract class AbstractIntegrationTest {
 
